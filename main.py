f491456f4626ff03a40ae3fad1782ac2343c899c
mahdishemshadi <mahdi.shemshadi@gmail.com>
2025-09-15 11:45:58 +0330

    - Add function pdf_exists - Add function text_formatter

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 import os
+import fitz
+import tqdm
 import requests
 
 file_name = "human-nutrition-text.pdf"
 file_url = "https://pressbooks.oer.hawaii.edu/humannutrition2/open/download?type=pdf"
 
-if not os.path.exists(file_name):
-    print("File doesn't exist, downloading...")
-    response = requests.get(file_url)
+def pdf_exists(file_path: str, file_link: str):
+    """
+    Check if pdf file exists. If file does not exist, download it.
 
-    if response.status_code == 200:
-        with open(file_name, "wb") as file:
-            file.write(response.content)
-            print(f"PDF file saved in {file_name}")
+    Args:
+         file_path(str): path of pdf file.
+         file_link(str): link to download pdf file.
+    """
 
+    if not os.path.exists(file_path):
+        print("File doesn't exist, downloading...")
+        response = requests.get(file_link)
+
+        if response.status_code == 200:
+            with open(file_name, "wb") as file:
+                file.write(response.content)
+                print(f"PDF file saved in {file_name}")
+
+        else:
+            print(f"PDF file could not be downloaded due to error: {response.status_code}")
     else:
-        print(f"PDF file could not be downloaded due to error: {response.status_code}")
-else:
-    print("File exists.")
+        print("File exists.")
 
+def text_formatter(text: str) -> str:
+    """
+    Replace all \n with space and delete all extra spaces from start and end of the text.
 
+    Args:
+         text(str): input text to format.
+
+    Return:
+        str: formatted text.
+    """
+
+    formatted_text = text.replace("\n", " ").strip()
+    return formatted_text
 
