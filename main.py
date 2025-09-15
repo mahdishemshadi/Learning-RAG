7151ba51cde3a28b30ac2a18a925252d7efd0b56
mahdishemshadi <mahdi.shemshadi@gmail.com>
2025-09-16 01:25:49 +0330

    - Add function read_pdf

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import fitz
-import tqdm
 import requests
 
+from tqdm.auto import tqdm
+
+
+page_offset = 41 # book main content starts from page 41 of pdf
 file_name = "human-nutrition-text.pdf"
 file_url = "https://pressbooks.oer.hawaii.edu/humannutrition2/open/download?type=pdf"
 
@@ -10,7 +13,7 @@ def pdf_exists(file_path: str, file_link: str):
     """
     Check if pdf file exists. If file does not exist, download it.
 
-    Args:
+    Params:
          file_path(str): path of pdf file.
          file_link(str): link to download pdf file.
     """
@@ -33,7 +36,7 @@ def text_formatter(text: str) -> str:
     """
     Replace all \n with space and delete all extra spaces from start and end of the text.
 
-    Args:
+    Params:
          text(str): input text to format.
 
     Return:
@@ -42,4 +45,28 @@ def text_formatter(text: str) -> str:
 
     formatted_text = text.replace("\n", " ").strip()
     return formatted_text
+
+
+def read_pdf(file_path: str) -> list[dict]:
+    """
+    Read pdf file page by page and extracts statistics.
+
+    Params:
+        file_path(str): path of the pdf file.
+
+    Returns:
+        list[dict]: extracted statistics containing page number, number of characters, number of words, number of
+        sentences, number of tokens and the whole formatted text.
+     """
+
+    result = list()
+    pdf = fitz.open(file_path)
+
+    for page_number, page in tqdm(enumerate(pdf)):
+        text = page.get_text()
+        text = text_formatter(text)
+        temp = {"page_number": page_number - page_offset,
+                "characters_count": len(text),
+                "words_count": len(text.split(" ")),
+                }
 
